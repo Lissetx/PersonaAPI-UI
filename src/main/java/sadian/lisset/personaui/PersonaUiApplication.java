16e125331d08b114f8e374eaa69087036e9a05bb
Lissetx <lissetsadian@hotmail.com>
2022-11-08 18:15:21 -0700

    Initial commit

---
+++ src/main/java/sadian/lisset/personaui/PersonaUiApplication.java
@@ -0,0 +1,13 @@
+package sadian.lisset.personaui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sadian/lisset/personaui/PersonaUiApplicationTests.java
@@ -0,0 +1,13 @@
+package sadian.lisset.personaui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
