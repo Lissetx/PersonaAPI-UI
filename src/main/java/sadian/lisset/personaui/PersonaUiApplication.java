d737cebde1eb8db6538533cc9c0136e7bad56595
Lissetx <98356241+Lissetx@users.noreply.github.com>
2022-11-28 23:57:41 -0700

    update create delete user

--- src/main/java/sadian/lisset/personaui/PersonaUiApplication.java
+++ src/main/java/sadian/lisset/personaui/PersonaUiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonaUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonaUiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaUiApplication.class, args);
+    }
 
 }
